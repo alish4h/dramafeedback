cd5362a5a5af20086cbc971f9a24c4f6b6e4b723
Taqi <Taqi@Alis-MacBook-Pro.local>
2018-05-07 18:53:36 +0100

    Successfuly ran the app on ios simulator

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,10 +10,6 @@
     </f7-navbar>
     <f7-block-title>{{ title }}</f7-block-title>
     <f7-block inner>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Distinctio est aliquam officiis quaerat placeat, cum explicabo magni
-      soluta totam maxime autem minima accusamus eos suscipit dignissimos
-      corporis modi voluptatum fugiat!
     </f7-block>
   </f7-page>
 </template>
@@ -22,7 +18,7 @@ export default {
   name: 'Home',
   data() {
     return {
-      title: 'Hello World'
+      title: 'Dramatic Feedback'
     };
   }
 };
